5786f07db50650749bac24039ce8954df1d14ef4
Carla Gouveia <carlajessica@gmail.com>
2011-08-05 15:37:30 -0300

    Implementing a page redirect after login and proper user instance.

--- sfDoctrineGuardPlugin/modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
+++ sfDoctrineGuardPlugin/modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
@@ -20,61 +20,26 @@ class BasesfGuardAuthActions extends sfActions
   public function executeSignin($request)
   {
     $user = $this->getUser();
-    if ($user->isAuthenticated())
-    {
+    if ($user->isAuthenticated()) {
       return $this->redirect('@homepage');
     }
-
-    $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin'); 
-    $this->form = new $class();
-
-    if ($request->isMethod('post'))
-    {
-      $this->form->bind($request->getParameter('signin'));
-      if ($this->form->isValid())
-      {
-        $values = $this->form->getValues(); 
-        $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
-
-        // always redirect to a URL set in app.yml
-        // or to the referer
-        // or to the homepage
-        $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
-
-        return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
-      }
-    }
-    else
-    {
-      if ($request->isXmlHttpRequest())
-      {
-        $this->getResponse()->setHeaderOnly(true);
-        $this->getResponse()->setStatusCode(401);
-
-        return sfView::NONE;
-      }
-
-      // if we have been forwarded, then the referer is the current URL
-      // if not, this is the referer of the current request
-      $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
-
-      $module = sfConfig::get('sf_login_module');
-      if ($this->getModuleName() != $module)
-      {
-        return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
-      }
-
-      $this->getResponse()->setStatusCode(401);
-    }
+    
+    sfGuardCAS::init();
+    phpCAS::forceAuthentication();
+    
+    $user = Doctrine::getTable('sfGuardUser')->retrieveByUsername(phpCAS::getUser());
+    $this->getUser()->signin($user, true);
+    
+    $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
+    
+    return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
   }
 
   public function executeSignout($request)
   {
     $this->getUser()->signOut();
-
-    $signoutUrl = sfConfig::get('app_sf_guard_plugin_success_signout_url', $request->getReferer());
-
-    $this->redirect('' != $signoutUrl ? $signoutUrl : '@homepage');
+    sfGuardCAS::init();
+    phpCAS::logout();
   }
 
   public function executeSecure($request)
